0624fe754ad0b712c9a4674615700e3201deff09
Francisco V <vxrnxk@gmail.com>
2020-03-08 12:51:02 -0300

    MVC Controllers

---
+++ aula-02/app/Http/Controllers/SeriesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index() {
+        $series = [
+            'Grey\'s Anatomy',
+            'Lost',
+            'Agents of SHIELD'
+        ];
+
+        $html = "<ul>";
+        foreach ($series as $serie) {
+            $html .= "<li>$serie</li>";
+        }
+        $html .= "</ul>";
+
+        return $html;
+    }
+}
